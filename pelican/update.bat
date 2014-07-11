0f85f88e066f6bfe4e761bcc8f56442e347d8db3
RL <li.rongcheng@gmail.com>
2014-07-11 14:36:08 +1000

    changed the output folder name from 'blog' to 'article'

--- pelican/update.bat
+++ pelican/update.bat
@@ -1,4 +1,4 @@
 :: if need to delete the output directory before updating, add a "-d" argument
 
-pelican article-src-en -o ../blog/en -s scripts/pelicanconf_en.py %1
-pelican article-src-zh -o ../blog/zh -s scripts/pelicanconf_zh.py %1
+pelican article-src-en -o ../article/en -s scripts/pelicanconf_en.py %1
+pelican article-src-zh -o ../article/zh -s scripts/pelicanconf_zh.py %1
